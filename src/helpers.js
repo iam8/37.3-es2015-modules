f62b45befcd09d42cd1d38eb41e988e3ce45f3da
alexm <alex-m@outlook.com>
2023-07-11 18:19:56 -0700

    Add stubs for required functions

---
+++ src/helpers.js
@@ -0,0 +1,23 @@
+// Ioana A Mititean
+// 37.3 - ES2015 Modules
+
+/**
+ * Array helper functions.
+ */
+
+
+/**
+ * Return a randomly-selected item from an array of items.
+ */
+function choice(items) {
+
+}
+
+
+/**
+ * Remove the first matching item from items, if the item exists, and return it. Otherwise, return
+ * undefined.
+ */
+function remove(items, item) {
+
+}
