3b530b1df81ec31eafc1e2847a3d78241d9ac2af
alexm <alex-m@outlook.com>
2023-07-11 18:46:06 -0700

    Add required operations and log messages

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 // Ioana A Mititean
 // 37.3 - ES2015 Modules
+
+/**
+ * Perform some operations using some imported items (ES2015 modules imports).
+ */
+
+import fruit from "./foods";
+import {choice, remove} from "./helpers";
+
+
+// Randomly draw a fruit from the array
+const fruitChoice = choice(fruit);
+
+// Log messages
+console.log(`I'd like one ${fruitChoice}, please.`);
+console.log(`Here you go: ${fruitChoice}.`);
+console.log("Delicious! May I have another?");
+
+// Remove the fruit from the array
+remove(fruit, fruitChoice);
+
+// Log final message
+console.log(`I'm sorry, we're all out. We have ${fruit} left.`);
