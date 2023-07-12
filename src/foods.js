b92f1088a75034a509b7994b4f90638dad8643b0
alexm <alex-m@outlook.com>
2023-07-11 18:22:36 -0700

    Create and export fruits array

---
+++ src/foods.js
@@ -0,0 +1,15 @@
+// Ioana A Mititean
+// 37.3 - ES2015 Modules
+
+/**
+ * An array of fruits.
+ */
+
+
+const fruits = [
+    "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🍎",
+    "🍏", "🍐", "🍒", "🍓", "🥝", "🍅", "🥑",
+];
+
+
+export default fruits;
